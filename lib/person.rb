2b71bcc610710de622c0dcc786b146414a8e3373
Sam Shearer <sshearer101@gmail.com>
2021-09-01 12:23:43 -0600

    commit tree e0343bfeaf65d88bbc6e96a7dbc51ab16d89a2f3

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,75 @@
-# your code goes here
+class Person
+
+    attr_reader :name, :happiness, :hygiene
+    attr_accessor :bank_account
+
+    def initialize(name)
+        @name = name
+        @bank_account = 25
+        @happiness = 8
+        @hygiene = 8
+    end
+
+    def happiness=(number)
+        @happiness = normalize(number)         
+    end
+
+    def hygiene=(number)
+        @hygiene = normalize(number)
+    end
+
+    def normalize(number)
+        
+        if number > 10
+            return 10    
+        elsif  number < 0
+            return  0
+        else 
+            return  number
+        end
+    end
+
+    def clean?
+        @hygiene > 7
+    end
+
+    def happy?
+        @happiness > 7
+    end
+
+    def get_paid(amount)
+        self.bank_account += amount
+        "all about the benjamins"
+    end
+
+    def take_bath
+        self.hygiene += 4
+        '♪ Rub-a-dub just relaxing in the tub ♫'
+    end
+
+    def work_out
+        self.happiness += 2
+        self.hygiene -= 3
+        "♪ another one bites the dust ♫"
+    end
+
+    def call_friend(friend)
+        self.happiness += 3
+        friend.happiness += 3
+        "Hi #{friend.name}! It's #{self.name}. How are you?"
+    end
+
+    def start_conversation(friend, topic)
+        if topic = "politics"
+            self.happiness -= 1
+            friend.happiness -= 1
+            return "blah blah partisan blah lobbyist"
+        elsif topic = "weather"
+            happiness += 1
+            return "blah blah sun blah rain"
+        else 
+            return "blah blah blah blah blah"
+        end
+    end
+end
+
